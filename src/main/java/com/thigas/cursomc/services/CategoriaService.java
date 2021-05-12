7b16e68bf2c175632efbe6bbbd7a0b4a118073b7
Thiago Claro <thiagocclaro@gmail.com>
2021-05-12 16:51:03 -0300

    paginação

--- src/main/java/com/thigas/cursomc/services/CategoriaService.java
+++ src/main/java/com/thigas/cursomc/services/CategoriaService.java
@@ -2,8 +2,12 @@ package com.thigas.cursomc.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thigas.cursomc.domain.Categoria;
@@ -44,6 +48,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linerPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linerPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
